fed3d415baea1989963d0007c0cce0872e00e93e
Chimezie Solomon <chimezie.solomon.cs@gmail.com>
2023-03-24 09:15:03 +0100

    new commit

--- site-seeing/src/App.js
+++ site-seeing/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import Load from './Load';
-import SitesSeeing from './SitesSeeing';
-
-
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from 'react'
+import Load from './Load'
+import SitesSeeing from './SitesSeeing'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-sites-project'
 
 function App() {
   const [load, setLoad] = useState(true)
   const [sitesSeeing, setSitesSeeing] = useState([])
 
-  const removeSite = (id) => {
-    const newSites = sitesSeeing.filter((SiteSeeing) => SiteSeeing.id !== id)
-    setSitesSeeing(newSites)
+  const removeSiteSeeing = (id) => {
+    const newSitesSeeing = sitesSeeing.filter((siteSeeing) => siteSeeing.id !== id)
+    setSitesSeeing(newSitesSeeing)
   }
 
-  const fetchSites = async () => {
+  const fetchSitesSeeing = async () => {
     setLoad(true)
     try {
       const response = await fetch(url)
@@ -25,24 +25,23 @@ function App() {
       setLoad(false)
       console.log(error)
     }
-
   }
   useEffect(() => {
-    fetchSites()
+    fetchSitesSeeing()
   }, [])
   if (load) {
     return (
       <main>
         <Load />
       </main>
-    );
+    )
   }
   if (sitesSeeing.length === 0) {
     return (
       <main>
         <div className='title'>
           <h2>no sites left</h2>
-          <button className='btn' onClick={() => fetchSites()}>
+          <button className='btn' onClick={() => fetchSitesSeeing()}>
             refresh
           </button>
         </div>
@@ -51,10 +50,10 @@ function App() {
   }
   return (
     <main>
-      <SitesSeeing sitesSeeing={sitesSeeing} removeSite={removeSite} />
+      <SitesSeeing sitesSeeing={sitesSeeing} removeSiteSeeing={removeSiteSeeing} />
     </main>
-  )
-  
-}
 
-export default App;
+  )
+  }
+
+export default App
--- site-seeing/src/SitesSeeing.js
+++ site-seeing/src/SitesSeeing.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import SiteSeeing from './SiteSeeing';
 
-const SitesSeeing = ({ sitesSeeing, removeSite }) => {
-    return (
-        <section>
-            <div className='title'>
-            <h2>our sites</h2>
-            <div className='underline'></div>
-            </div>
-            <div>
-                {sitesSeeing.map((siteSeeing) => {
-                    return <SiteSeeing key={siteSeeing.id} {...siteSeeing} removeSite={removeSite} />
-                })}
-            </div>
-        </section>
-    );
+
+const SitesSeeing = ({ sitesSeeing, removeSiteSeeing }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our sites</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {sitesSeeing.map((siteSeeing) => {
+          return <SiteSeeing key={siteSeeing.id} {...siteSeeing} removeSiteSeeing={removeSiteSeeing} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default SitesSeeing;
